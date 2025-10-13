592b2ebec93c8c6438815e37ad5d6ab3966399bd
amov-nuc-gpu <fatmoonqyp@126.com>
2025-10-13 14:45:46 +0800

    update competition

--- Simulation/sysu_competition/src/gazebo_model.h
+++ Simulation/sysu_competition/src/gazebo_model.h
@@ -1,7 +1,11 @@
 #include "ros_msg_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define FLIP_ANGLE M_PI/3
 #define RC_DEADZONE 0.2
+#define MAX_DATA_SIZE 2000
 
 class Gazebo_model
 {
@@ -10,18 +14,48 @@ private:
     int uav_id;             // 【参数】无人机ID
     std::string uav_name;   // 【参数】无人机名称
 
+    double mission_time{0.0};
+
+    // 定义数据结构体
+    struct DataPoint{
+        double time;        // 时间（秒）
+        double x;           // x位置（米）
+        double y;           // y位置（米）
+        double z;           // z位置（米）
+        double yaw;         // 偏航角（弧度）
+    };
+
+    struct DataSet{
+        DataPoint points[MAX_DATA_SIZE];
+        int count{0};
+    };
+
+
+    DataSet uav1_data;
+    DataSet uav2_data;
+    DataSet uav3_data;
+
     // 无人机飞行相关参数
     struct Gazebomodel
     {
         string model_name;
-        Eigen::Vector3d init_pos{0.0, 0.0, 0.0};     // 降落点
+        Eigen::Vector3d init_pos{0.0, 0.0, 0.0};     
         double init_yaw{0.0};
-        Eigen::Vector3d model_pos{0.0, 0.0, 0.0};     // 降落点
+        bool get_init_pose{false};
+        Eigen::Vector3d model_pos{0.0, 0.0, 0.0};  
         double model_yaw{0.0}; 
+        Eigen::Vector3d model_vel{0.0, 0.0, 0.0};     
+        double yaw_vel{0.0};
+        Eigen::Vector3d set_pos{0.0, 0.0, 0.0};  
+        double set_yaw{0.0}; 
     };
     Gazebomodel uav1;
     Gazebomodel uav2;
     Gazebomodel uav3;
+
+    Gazebomodel ugv1;
+    Gazebomodel ugv2;
+    Gazebomodel ugv3;
 
     // 订阅句柄
     ros::Subscriber uav1_gazebo_pose_sub;
@@ -31,7 +65,18 @@ private:
     void uav2_gazebo_pose_cb(const nav_msgs::Odometry::ConstPtr &msg);  
     void uav3_gazebo_pose_cb(const nav_msgs::Odometry::ConstPtr &msg);  
 
+    // 订阅句柄
+    ros::Subscriber ugv1_gazebo_pose_sub;
+    ros::Subscriber ugv2_gazebo_pose_sub;
+    ros::Subscriber ugv3_gazebo_pose_sub;
+    void ugv1_gazebo_pose_cb(const nav_msgs::Odometry::ConstPtr &msg);  
+    void ugv2_gazebo_pose_cb(const nav_msgs::Odometry::ConstPtr &msg);  
+    void ugv3_gazebo_pose_cb(const nav_msgs::Odometry::ConstPtr &msg); 
 
+
+    void set_uav1_vel(double vel_x, double vel_y, double vel_z, double vel_yaw);
+    int read_data_from_file(const char* filename, DataSet* data);
+    int find_nearest_time_index(const DataSet* dataset, double target_time);
 
     ros::Publisher model_state_pub;    // 【订阅】控制指令订阅
 
@@ -42,6 +87,7 @@ public:
 
     void init(ros::NodeHandle &nh); 
     void debug();
+    void main_loop();
 
     void set_model_state(string model_name, double x, double y, double z, double yaw);
 
--- Simulation/sysu_competition/src/gazebo_model_node.cpp
+++ Simulation/sysu_competition/src/gazebo_model_node.cpp
@@ -18,14 +18,17 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "gazebo_model_node");
     ros::NodeHandle nh("~");
-    ros::Rate rate(50.0);
+    ros::Rate rate(20.0);
 
     // 中断信号注册 
     signal(SIGINT, mySigintHandler);
 
     // 读取参数
     bool flag_printf; // 是否打印状态
-    nh.param<bool>("flag_printf", flag_printf, true);           
+    nh.param<bool>("flag_printf", flag_printf, true); 
+
+
+    sleep(5.0);          
 
     // 初始化外部估计类
     Gazebo_model test;
@@ -37,13 +40,15 @@ int main(int argc, char **argv)
     {
         ros::spinOnce();
 
+        // 定时主循环更新
+        test.main_loop();
+
         // 定时打印状态
         if (ros::Time::now() - last_time > ros::Duration(1.0) && flag_printf)
         {
             test.debug();
-            test.set_model_state("uav_1", 10,7.0,5,0.0/180.0*M_PI);
-            test.set_model_state("uav_2", 10,9.0,5,90.0/180.0*M_PI);
-            test.set_model_state("uav_3", 10,8.0,5,180.0/180.0*M_PI);
+            // test.set_model_state("uav_2", 10,9.0,5,90.0/180.0*M_PI);
+            // test.set_model_state("uav_3", 10,8.0,5,180.0/180.0*M_PI);
             last_time = ros::Time::now();
         }
 
